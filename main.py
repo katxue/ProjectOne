6c37ca73edefa0cd4f7a47898e983eecdbb35881
Fedya <fedsemenov52@gmail.com>
2021-06-18 23:35:02 -0700

    6/18/21

--- main.py
+++ main.py
@@ -9,28 +9,38 @@ clock = pygame.time.Clock()
 FPS = 60
 
 
-def calc_base(start, base_height, base_width, color):
+def to_ints(coords):
+    return [(int(c[0]), int(c[1])) for c in coords]
+
+
+def calc_base(start, base_width, base_height, color):
     top_left = (start[0], start[1] - base_height)
     rect = (top_left, (base_width, base_height))
     return color, rect
 
 
-def draw_base(surface, start, base_height, base_width):
-    color, rect = calc_base(start, base_height, base_width, (255, 255, 255))
+def calc_trapezoid(start, seg_wt, seg_wb, seg_height, base_width, base_height):
+    x = start[0] + ((base_width - seg_wb)/2)
+    y = start[1] - base_height
+    trapezoid = ((x, y),  # Bottom left
+                 (seg_wb + x, y),  # Bottom right
+                 (x + ((seg_wb + seg_wt) / 2), y - seg_height),  # Top right
+                 (x + ((seg_wb - seg_wt) / 2), y - seg_height))  # Top left
+
+    return trapezoid
+
+
+def draw_base(surface, start, base_width, base_height):
+    color, rect = calc_base(start, base_width, base_height, (255, 255, 255))
     pygame.draw.rect(surface, color, rect)
 
-def draw_top(surface, start, num_segments, seg_wt, seg_wb, seg_height, seg_scale, base_height, base_width):
-    start = (start[0], start[1])
-    x = start[0] - seg_wt/2
-    y = start[1] - base_height - seg_height
-    trapezoid = (x, y), \
-                (seg_wt + x, y), \
-                ((seg_wb - seg_wt) / 2 + seg_wt + x , seg_height + y), \
-                (x - ((seg_wb - seg_wt) / 2), seg_height + y)
+
+def draw_top(surface, start, num_segments, seg_wt, seg_wb, seg_height, seg_scale, base_width, base_height):
+    trapezoid = calc_trapezoid(start, seg_wt, seg_wb, seg_height, base_width, base_height)
     pygame.draw.polygon(surface, (255, 255, 255), trapezoid, 3)
 
 
-def draw_tree(surface: pygame.Surface, start: (int, int), base_height: int, base_width: int, num_segments: int,
+def draw_tree(surface: pygame.Surface, start: (int, int), base_width: int, base_height: int, num_segments: int,
               seg_wt: int, seg_wb: int, seg_height: int, seg_scale: float):
     """
     :param surface: pygame.Surface
@@ -45,8 +55,8 @@ def draw_tree(surface: pygame.Surface, start: (int, int), base_height: int, base
     :return: None
     """
 
-    draw_base(surface, start, base_height, base_width)
-    draw_top(surface, start, num_segments, seg_wt, seg_wb, seg_height, seg_scale, base_height, base_width)
+    draw_base(surface, start, base_width, base_height)
+    draw_top(surface, start, num_segments, seg_wt, seg_wb, seg_height, seg_scale, base_width, base_height)
 
 
 def main(screen, width, height):
@@ -61,12 +71,9 @@ def main(screen, width, height):
                 height = event.h
                 screen = pygame.display.set_mode((width, height), flags=pygame.RESIZABLE)
 
-        draw_tree(screen, (int(width/2), int(height/2)), 100, 30, 0, 0, 0, 0, 0)
+        draw_tree(screen, (int(width/2), int(height/2)), 30, 100, 0, 80, 220, 50, 0)
         pygame.display.flip()
 
 
-      draw_tree(screen, (int(width/2), int(height/2)), 100, 30, 0 , 80, 220, 50, 0)
-      pygame.display.flip()
-    
 if __name__ == '__main__':
     main(screen, width, height)
